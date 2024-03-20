94ed75c764051e2adef3858887d9b7d27f75ac71
Rahman <rahman@192.168.1.18>
2024-03-21 00:56:15 +0700

    ganti judul

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title>SnapTik: Tiktok Downloader Tools - Download videos Tiktok Without Watermark for Free</title>
+<title>SnapTik: Tiktok Downloader Tools - Download videos Tiktok Without Watermark for Free !</title>
 <meta name="robots" content="index,follow">
 <meta name="revisit-after" content="1 days">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5, shrink-to-fit=no">
